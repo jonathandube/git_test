f0c24d44dea6f53fa5e5216c6cb89f7e1a53bb29
jodub_2k33v3 <jodub_2k33v3@JONATHANLAPTOP>
2017-08-31 14:28:32 -0400

    test#1

--- git_test/Program.cs
+++ git_test/Program.cs
@@ -10,6 +10,12 @@ namespace git_test
     {
         static void Main(string[] args)
         {
+
+            string name;
+
+            Console.WriteLine("whats ur name bruh : ");
+            Console.ReadLine();
+
         }
     }
 }
